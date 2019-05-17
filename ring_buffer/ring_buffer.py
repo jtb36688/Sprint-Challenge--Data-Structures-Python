75e5e3b14cf21a9338d9fceb6a0a5ecaf6c312cb
Jacob Bryan <jtb36688@gmail.com>
2019-05-17 11:34:48 -0400

    working on the ring buffer problem

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,20 @@
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
-    self.storage = []
+    self.storage = [None]*capacity
 
   def append(self, item):
+    print("running append")
     if len(self.storage) == self.capacity:
-      
+      self.storage[-1] = None
+      self.storage[0] = item
+    else:
+      self.storage[0] = item
+
+
 
   def get(self):
-    pass
+    orderedlist = [None]*self.capacity
+    for i in range(len(self.storage)-1):
+      orderedlist.insert(0, self.storage[i+1])
+    return orderedlist
